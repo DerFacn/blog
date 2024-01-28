d02f20ecd0a9cd1827d0db2692245c0a2183cb01
DerFacn <nikitalupalo145@gmail.com>
2024-01-29 00:06:17 +0100

    User profile page created

--- app/general/__init__.py
+++ app/general/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from .routes import index
+from .routes import index, me
 
 bp = Blueprint('general', __name__)
 
 bp.add_url_rule('/', 'index', index, methods=['GET'])
+bp.add_url_rule('/me', 'me', me, methods=['GET'])
--- app/general/routes.py
+++ app/general/routes.py
@@ -1,11 +1,16 @@
 from flask import render_template
-from app.utils import get_user
+from app.utils import get_user, login_required
 from app.db import session
 from app.models import Post
 
 
-@get_user
-def index(user):
+def index():
     all_posts = session.query(Post).all()
     posts = all_posts[::-1]
-    return render_template('index.html', user=user, posts=posts)
+    return render_template('index.html', posts=posts)
+
+
+@login_required
+def me(user):
+    my_posts = session.query(Post).filter_by(user_uuid=user.uuid).all()
+    return render_template('me.html', title='Me', user=user, posts=my_posts)
