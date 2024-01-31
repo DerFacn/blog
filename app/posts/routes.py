56634573194dc35cbc9c798e49c9d0a5b1f95d15
DerFacn <nikitalupalo145@gmail.com>
2024-01-31 22:42:06 +0100

    Posts editing added + 403 error handler

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -24,6 +24,26 @@ def create_post(user):
 
 
 @login_required
+def edit_post(user, post_id):
+    post = session.query(Post).filter_by(id=post_id).first()
+    if not post:
+        abort(404)
+    elif post.user_uuid != user.uuid:
+        abort(403)
+    form = PostForm(title=post.title, body=post.body)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            title = request.form.get('title')
+            body = request.form.get('body')
+            post.title = title
+            post.body = body
+            session.commit()
+            return redirect(url_for('posts.view_post', post_id=post.id))
+    return render_template('posts/create.html', title='Edit post', form=form)
+
+
+
+@login_required
 def delete_post(user, post_id):
     post = session.query(Post).filter_by(id=int(post_id)).first()
     session.delete(post)
