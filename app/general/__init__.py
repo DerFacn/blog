b49152f874e122ac52f15779eb87a103d16c6263
DerFacn <nikitalupalo145@gmail.com>
2024-02-01 17:39:17 +0100

    Search function created

--- app/general/__init__.py
+++ app/general/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from .routes import index
+from .routes import index, search
 
 bp = Blueprint('general', __name__)
 
 bp.add_url_rule('/', 'index', index, methods=['GET'])
+bp.add_url_rule('/search', 'search', search, methods=['GET'])
